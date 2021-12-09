1e7593c9c602bfe33586fe753e26f85b7d562fd6
Bwaim <fabien.boismoreau@gmail.com>
2021-12-09 19:41:06 +0100

    bump versions

--- app/src/main/java/dev/bwaim/loteria/app/LoteriaApplication.kt
+++ app/src/main/java/dev/bwaim/loteria/app/LoteriaApplication.kt
@@ -1,7 +1,14 @@
 package dev.bwaim.loteria.app
 
 import android.app.Application
+import coil.ImageLoaderFactory
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-internal class LoteriaApplication : Application()
+internal class LoteriaApplication : Application(), ImageLoaderFactory {
+    @Inject
+    lateinit var imageLoaderFactory: ImageLoaderFactory
+
+    override fun newImageLoader() = imageLoaderFactory.newImageLoader()
+}

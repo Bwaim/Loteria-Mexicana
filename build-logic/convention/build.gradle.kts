aa3750609e11a3fc736930e068a2b58cfbd55940
Fabien Boismoreau <fabien.boismoreau@gmail.com>
2022-06-07 00:27:55 +0200

    Update project configuration

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -14,3 +14,44 @@ dependencies {
     implementation(libs.kotlin.gradle)
     implementation(libs.spotless.gradlePlugin)
 }
+
+gradlePlugin {
+    plugins {
+        register("androidApplicationCompose") {
+            id = "loteriamexicana.android.application.compose"
+            implementationClass = "AndroidApplicationComposeConventionPlugin"
+        }
+        register("androidApplication") {
+            id = "loteriamexicana.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+        register("androidApplicationJacoco") {
+            id = "loteriamexicana.android.application.jacoco"
+            implementationClass = "AndroidApplicationJacocoConventionPlugin"
+        }
+        register("androidLibraryCompose") {
+            id = "loteriamexicana.android.library.compose"
+            implementationClass = "AndroidLibraryComposeConventionPlugin"
+        }
+        register("androidLibrary") {
+            id = "loteriamexicana.android.library"
+            implementationClass = "AndroidLibraryConventionPlugin"
+        }
+        register("androidFeature") {
+            id = "loteriamexicana.android.feature"
+            implementationClass = "AndroidFeatureConventionPlugin"
+        }
+        register("androidLibraryJacoco") {
+            id = "loteriamexicana.android.library.jacoco"
+            implementationClass = "AndroidLibraryJacocoConventionPlugin"
+        }
+        register("androidTest") {
+            id = "loteriamexicana.android.test"
+            implementationClass = "AndroidTestConventionPlugin"
+        }
+        register("spotless") {
+            id = "loteriamexicana.spotless"
+            implementationClass = "SpotlessConventionPlugin"
+        }
+    }
+}

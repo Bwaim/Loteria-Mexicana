2de9f986f0f1b2d723fc7d0b8679af084e75e3a6
Fabien Boismoreau <fabien.boismoreau@gmail.com>
2023-02-12 04:15:25 +0100

    Fix compile java version

--- build-logic/convention/src/main/kotlin/dev.bwaim.loteria/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/dev.bwaim.loteria/KotlinAndroid.kt
@@ -23,8 +23,8 @@ fun Project.configureKotlinAndroid(
         }
 
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
             isCoreLibraryDesugaringEnabled = true
         }
 
@@ -45,8 +45,8 @@ fun Project.configureKotlinAndroid(
                 "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api"
             )
 
-            // Set JVM target to 1.8
-            jvmTarget = JavaVersion.VERSION_1_8.toString()
+            // Set JVM target to 11
+            jvmTarget = JavaVersion.VERSION_11.toString()
         }
     }
 
